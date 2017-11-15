551910ece7b78deefb7dcf861e0b92c6d7973fab
Xiaodong <2014060104022@std.uestc.edu.cn>
2017-11-15 21:59:34 +0800

    Update and rename ont_hot.py to one_hot.py

--- ont_hot.py
+++
@@ -1,6 +0,0 @@
-data, target, otherfea = next(iter(train_loader))
-data = torch.autograd.Variable(data.cuda())
-target = torch.autograd.Variable(target.cuda())
-one_hot = torch.rand(target.size(0),nlabels).zero_()
-one_hot.scatter_(1, target.view(target.size(0),1).data.cpu(), 1)
-one_hot = torch.autograd.Variable(one_hot.cuda())
